78f7e5c6fdf700bb19f450c473780448c5e13a38
Audiakshay <121844200+Audiakshay@users.noreply.github.com>
2023-01-28 10:53:42 +0530

    setup done

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './public/index.html',
+    filename: 'index.html',
+  })],
+  mode: 'development',
+  devServer: {
+    port: '3004',
+    open: true,
+  },
+
+};
